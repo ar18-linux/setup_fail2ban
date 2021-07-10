c1392e8e901473f113b8157610385609db605658
nulysses <adr.zivil@gmail.com>
2021-07-10 11:28:17 +0200

    1

--- setup_fail2ban/setup_fail2ban.sh
+++ setup_fail2ban/setup_fail2ban.sh
@@ -3,9 +3,18 @@
 
 # Prepare script environment
 {
-  # Script template version 2021-07-04_19:11:04
+  # Script template version 2021-07-10_11:24:30
+  # Get old shell option values to restore later
+  shopt -s inherit_errexit
+  IFS=$'\n' shell_options=($(shopt -op))
+  set +x
+  # Set shell options for this script
+  set -o pipefail
+  set -e
   # Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
+  set +u
   LD_PRELOAD_old="${LD_PRELOAD}"
+  set -u
   LD_PRELOAD=
   # Determine the full path of the directory this script is in
   script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
@@ -31,14 +40,8 @@
     declare -A -g ar18_pwd_map
   fi
   ar18_pwd_map["${script_path}"]="${PWD}"
-  # Get old shell option values to restore later
-  shopt -s inherit_errexit
-  IFS=$'\n' shell_options=($(shopt -op))
-  # Set shell options for this script
-  set -o pipefail
-  set -eu
   if [ ! -v ar18_parent_process ]; then
-    export ar18_parent_process="$"
+    export ar18_parent_process="$$"
   fi
   # Get import module
   if [ ! -v ar18.script.import ]; then
@@ -73,15 +76,15 @@ ar18.script.execute_with_sudo sed -i "s/{{port}}/${ar18_port}/g" "/etc/fail2ban/
 ##################################SCRIPT_END###################################
 # Restore environment
 {
-  # Restore old shell values
   set +x
-  for option in "${shell_options[@]}"; do
-    eval "${option}"
-  done
   # Restore LD_PRELOAD
   LD_PRELOAD="${LD_PRELOAD_old}"
   # Restore PWD
   cd "${ar18_pwd_map["${script_path}"]}"
+  # Restore old shell values
+  for option in "${shell_options[@]}"; do
+    eval "${option}"
+  done
 }
 # Return or exit depending on whether the script was sourced or not
 {
