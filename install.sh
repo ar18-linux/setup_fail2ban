8dfca1b231d3da11cddf7d4cd6a3fcb50cfaef05
nulysses <adr.zivil@gmail.com>
2021-07-10 11:08:55 +0200

    1

---
+++ install.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+# ar18
+
+# Prepare script environment
+{
+  # Script template version 2021-07-09_20:12:35
+  # Get old shell option values to restore later
+  shopt -s inherit_errexit
+  IFS=$'\n' shell_options=($(shopt -op))
+  # Set shell options for this script
+  set -o pipefail
+  set -ex
+  # Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
+  LD_PRELOAD_old="${LD_PRELOAD}"
+  set -u
+  LD_PRELOAD=
+  # Determine the full path of the directory this script is in
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+  script_path="${script_dir}/$(basename "${0}")"
+  #Set PS4 for easier debugging
+  export PS4='\e[35m${BASH_SOURCE[0]}:${LINENO}: \e[39m'
+  # Determine if this script was sourced or is the parent script
+  if [ ! -v ar18_sourced_map ]; then
+    declare -A -g ar18_sourced_map
+  fi
+  if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    ar18_sourced_map["${script_path}"]=1
+  else
+    ar18_sourced_map["${script_path}"]=0
+  fi
+  # Initialise exit code
+  if [ ! -v ar18_exit_map ]; then
+    declare -A -g ar18_exit_map
+  fi
+  ar18_exit_map["${script_path}"]=0
+  # Save PWD
+  if [ ! -v ar18_pwd_map ]; then
+    declare -A -g ar18_pwd_map
+  fi
+  ar18_pwd_map["${script_path}"]="${PWD}"
+  if [ ! -v ar18_parent_process ]; then
+    export ar18_parent_process="$$"
+  fi
+  # Get import module
+  if [ ! -v ar18.script.import ]; then
+    mkdir -p "/tmp/${ar18_parent_process}"
+    cd "/tmp/${ar18_parent_process}"
+    curl -O https://raw.githubusercontent.com/ar18-linux/ar18_lib_bash/master/ar18_lib_bash/script/import.sh > /dev/null 2>&1 && . "/tmp/${ar18_parent_process}/import.sh"
+    cd "${ar18_pwd_map["${script_path}"]}"
+  fi
+}
+#################################SCRIPT_START##################################
+
+ar18.script.import ar18.script.install
+ar18.script.import ar18.script.read_target
+
+. "${script_dir}/vars"
+
+ar18.script.install "${install_dir}" "${module_name}" "${script_dir}"
+
+set +u
+ar18_deployment_target="$(ar18.script.read_target "${1}")"
+set -u
+
+"${script_dir}/setup_ssh_server/setup_ssh_server.sh" "${ar18_deployment_target}"
+
+##################################SCRIPT_END###################################
+# Restore environment
+{
+  set +x
+  # Restore LD_PRELOAD
+  LD_PRELOAD="${LD_PRELOAD_old}"
+  # Restore PWD
+  cd "${ar18_pwd_map["${script_path}"]}"
+  # Restore old shell values
+  for option in "${shell_options[@]}"; do
+    eval "${option}"
+  done
+}
+# Return or exit depending on whether the script was sourced or not
+{
+  if [ "${ar18_sourced_map["${script_path}"]}" = "1" ]; then
+    return "${ar18_exit_map["${script_path}"]}"
+  else
+    exit "${ar18_exit_map["${script_path}"]}"
+  fi
+}
---
+++ setup_fail2ban/setup_fail2ban.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+# ar18
+
+# Prepare script environment
+{
+  # Script template version 2021-07-04_19:11:04
+  # Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
+  LD_PRELOAD_old="${LD_PRELOAD}"
+  LD_PRELOAD=
+  # Determine the full path of the directory this script is in
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+  script_path="${script_dir}/$(basename "${0}")"
+  #Set PS4 for easier debugging
+  export PS4='\e[35m${BASH_SOURCE[0]}:${LINENO}: \e[39m'
+  # Determine if this script was sourced or is the parent script
+  if [ ! -v ar18_sourced_map ]; then
+    declare -A -g ar18_sourced_map
+  fi
+  if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    ar18_sourced_map["${script_path}"]=1
+  else
+    ar18_sourced_map["${script_path}"]=0
+  fi
+  # Initialise exit code
+  if [ ! -v ar18_exit_map ]; then
+    declare -A -g ar18_exit_map
+  fi
+  ar18_exit_map["${script_path}"]=0
+  # Save PWD
+  if [ ! -v ar18_pwd_map ]; then
+    declare -A -g ar18_pwd_map
+  fi
+  ar18_pwd_map["${script_path}"]="${PWD}"
+  # Get old shell option values to restore later
+  shopt -s inherit_errexit
+  IFS=$'\n' shell_options=($(shopt -op))
+  # Set shell options for this script
+  set -o pipefail
+  set -eu
+  if [ ! -v ar18_parent_process ]; then
+    export ar18_parent_process="$"
+  fi
+  # Get import module
+  if [ ! -v ar18.script.import ]; then
+    mkdir -p "/tmp/${ar18_parent_process}"
+    cd "/tmp/${ar18_parent_process}"
+    curl -O https://raw.githubusercontent.com/ar18-linux/ar18_lib_bash/master/ar18_lib_bash/script/import.sh > /dev/null 2>&1 && . "/tmp/${ar18_parent_process}/import.sh"
+    cd "${ar18_pwd_map["${script_path}"]}"
+  fi
+}
+#################################SCRIPT_START##################################
+
+ar18.script.import ar18.script.install
+ar18.script.import ar18.script.read_target
+ar18.script.import ar18.pacman.install
+ar18.script.import ar18.script.obtain_sudo_password
+ar18.script.import ar18.script.execute_with_sudo
+
+set +u
+ar18_deployment_target="$(ar18.script.read_target "${1}")"
+set -u
+
+ar18.script.source_or_execute_config "source" "setup_fail2ban" "${ar18_deployment_target}"
+
+ar18.script.obtain_sudo_password
+
+ar18.pacman.install fail2ban
+ar18.script.execute_with_sudo cp -f "${script_dir}/jail_file" "/etc/fail2ban/jail.local"
+ar18.script.execute_with_sudo chown root:root "/etc/fail2ban/jail.local"
+ar18.script.execute_with_sudo chmod 644 "/etc/fail2ban/jail.local"
+ar18.script.execute_with_sudo sed -i "s/{{port}}/${ar18_port}/g" "/etc/fail2ban/jail.local"
+
+##################################SCRIPT_END###################################
+# Restore environment
+{
+  # Restore old shell values
+  set +x
+  for option in "${shell_options[@]}"; do
+    eval "${option}"
+  done
+  # Restore LD_PRELOAD
+  LD_PRELOAD="${LD_PRELOAD_old}"
+  # Restore PWD
+  cd "${ar18_pwd_map["${script_path}"]}"
+}
+# Return or exit depending on whether the script was sourced or not
+{
+  if [ "${ar18_sourced_map["${script_path}"]}" = "1" ]; then
+    return "${ar18_exit_map["${script_path}"]}"
+  else
+    exit "${ar18_exit_map["${script_path}"]}"
+  fi
+}
